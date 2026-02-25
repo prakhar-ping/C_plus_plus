7c2df29c6da79e5701e38fca8a8ac489657eed8a
Prakhar Verma <prakharverma@Prakhars-Mac-mini.local>
2026-02-26 01:58:10 +0530

    Modified Templates class to operator overloading

--- Templates.cpp
+++ Templates.cpp
@@ -13,6 +13,16 @@ struct Vector2
     {
         return (x * other.x) + (y * other.y);
     }
+
+    Vector2<T> operator+(const Vector2<T> &rhs) const
+    {
+        return Vector2<T>(x + rhs.x, y + rhs.y);
+    }
+
+    Vector2<T> operator*(const T &val) const
+    {
+        return Vector2<T>(x * val, y * val);
+    }
 };
 
 int main()
@@ -22,7 +32,10 @@ int main()
     Vector2<float> v3(2.3, 3.2);
     Vector2<float> v4(4.5, 5.4);
 
-    std::cout << v1.DotProduct(v2) << std::endl;
+    Vector2<int> v5 = v1 + v2;
+    v5 = v5 * 2;
+    std::cout << v5.x << std::endl;
+    std::cout << v5.y << std::endl;
     std::cout << v3.DotProduct(v4) << std::endl;
     return 0;
 }
