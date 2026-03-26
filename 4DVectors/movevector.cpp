56a17ed4d83af2237c52c266743384fe6d0c72ce
Prakhar Verma <v.rvrudra05066@gmail.com>
2026-03-27 01:15:07 +0530

    Update coordinate calculation for 4D movement

--- 4DVectors/movevector.cpp
+++ 4DVectors/movevector.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 // alignas(16) forces the OS to place this struct at a memory address
 // that is a perfect multiple of 16 bytes. This is a strict hardware requirement
@@ -37,19 +38,87 @@ struct alignas(16) Matrix4X4
         result.z = (m[8] * v.x) + (m[9] * v.y) + (m[10] * v.z) + (m[11] * v.w);
         result.w = (m[12] * v.x) + (m[13] * v.y) + (m[14] * v.z) + (m[15] * v.w);
         return result;
-    };
+    }
+
+    Matrix4X4 operator*(const Matrix4X4 &mat) const
+    {
+        Matrix4X4 result;
+        for (int row = 0; row < 4; ++row)
+        {
+            for (int col = 0; col < 4; ++col)
+            {
+                result.m[(row * 4) + col] =
+                    (m[(row * 4) + 0] * mat.m[(0 * 4) + col]) +
+                    (m[(row * 4) + 1] * mat.m[(1 * 4) + col]) +
+                    (m[(row * 4) + 2] * mat.m[(2 * 4) + col]) +
+                    (m[(row * 4) + 3] * mat.m[(3 * 4) + col]);
+            }
+        }
+        return result;
+    }
+
+    static Matrix4X4 ScaleMatrix(float x, float y, float z)
+    {
+        Matrix4X4 result;
+        result.m[0] = x;
+        result.m[5] = y;
+        result.m[10] = z;
+        return result;
+    }
+    static Matrix4X4 TranslateMatrix(float x, float y, float z)
+    {
+        Matrix4X4 result;
+        result.m[3] = x;
+        result.m[7] = y;
+        result.m[11] = z;
+        return result;
+    }
+
+    static Matrix4X4 RotateY(float radians)
+    {
+        Matrix4X4 result; // Starts as Identity Matrix
+        float c = std::cos(radians);
+        float s = std::sin(radians);
+
+        result.m[0] = c;
+        result.m[2] = s;
+        result.m[8] = -s;
+        result.m[10] = c;
+
+        return result;
+    }
 };
+
+void PrintMatrix(const Matrix4X4 &mat)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        if (i % 4 == 0)
+        {
+            std::cout << std::endl;
+        }
+        std::cout << mat.m[i] << ",";
+    }
+}
 
 int main()
 {
-    Vector4 spaceShip(0, 0, 0, 1.0f);
-    Matrix4X4 targetMatrix;
-    targetMatrix.m[3] = 10.0f;
-    targetMatrix.m[7] = 0.0f;
-    targetMatrix.m[11] = 50.0f;
+    Vector4 vertex(1.0f, 0.0f, 0.0f, 1.0f);
+    float ang = 3.14159f;
+    Matrix4X4 targetMatrix = Matrix4X4::TranslateMatrix(10.0f, 0.0f, 50.0f);
+    Matrix4X4 scaleTargetMatrix = Matrix4X4::ScaleMatrix(2.0f, 1.0f, 1.0f);
+    Matrix4X4 rotateMatrix = Matrix4X4::RotateY(ang);
 
-    Vector4 targetMovement = targetMatrix * spaceShip;
-    std::cout << "BeforMovement -> " << spaceShip.x << "," << spaceShip.y << "," << spaceShip.z << "," << spaceShip.w << std::endl;
-    std::cout << "AfterMovement -> " << targetMovement.x << "," << targetMovement.y << "," << targetMovement.z << "," << targetMovement.w << std::endl;
+    Matrix4X4 superMatrix = targetMatrix * rotateMatrix * scaleTargetMatrix;
+    Vector4 newVector = superMatrix * vertex;
+    PrintMatrix(targetMatrix);
+    std::cout << std::endl;
+    PrintMatrix(scaleTargetMatrix);
+    std::cout << std::endl;
+    PrintMatrix(rotateMatrix);
+    std::cout << std::endl;
+    PrintMatrix(superMatrix);
+    std::cout << std::endl;
+    std::cout << "Final vector ->" << newVector.x << "," << newVector.y << "," << newVector.z << "," << newVector.w << std::endl;
     return 0;
 }
